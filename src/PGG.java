8d641c5fea497bbd508228ef396ecc7c2ff2beb6
diogo <digasnikas@gmail.com>
2016-11-22 18:04:13 +0000

    PGG

---
+++ src/PGG.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+
+
+public class PGG {
+
+    private List<Integer> myList;
+
+    //constructor
+    public PGG(){
+
+    }
+
+    public static void main(String[] args){
+        PGG myPGG = new PGG();
+        myPGG.DoSmth();
+    }
+
+    private void DoSmth(){
+        myList.add(1);
+    }
+
+}
